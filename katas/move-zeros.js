f633689f43eff489877f86fe3d90ac573fc8c10c
sandrabosk <sandraboskovic@hotmail.com>
2021-06-26 17:29:53 +0200

    add kata

--- katas/move-zeros.js
+++ katas/move-zeros.js
@@ -6,7 +6,6 @@
 // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 // ************************************************************************************
 
-//////// my solution: ////////
 function moveZeros(arr) {
   const result = [];
   const zeros = [];
